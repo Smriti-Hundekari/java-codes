89883ddf1042f3de387f214a7fdc627a1efc5b96
smriti <hundekarismriti@gmail.com>
2025-08-08 14:40:50 +0530

    Variables in Java

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String a[]){
+        int num1 = 3;
+        int num2 = 4;
+        int result = num1 + num2;
+        System.out.println(result);
+
+    }
+}
+
+//num1 and num2 are the variable names
+//syntax - 
