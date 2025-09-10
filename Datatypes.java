95722eb30d57ed3d18769c3ef84a32b032ca8d21
smriti <hundekarismriti@gmail.com>
2025-09-10 22:57:17 +0530

    DataTypes

---
+++ Datatypes.java
@@ -0,0 +1,32 @@
+public class Datatypes {
+    public static void main(String a[])
+    {
+        int num1 = 7;
+        byte b = 5;
+        short s = 4;
+        long l = 2345l;
+        float f = 7.5f;
+        double d = 7.5;
+        char c = 's';
+        boolean b2 = true;
+
+        System.out.println(num1 + ", " + b + ", " + s + ", " + l + ", " + f + ", " + d + ", " + c + ", " + b2);
+    }
+}
+
+
+//                         Datatypes
+//                         /       
+//                    Primitive 
+//                    Integer - byte,short,int,long
+//                    Float   - double,float
+//                    Character
+//                    Boolean
+
+//  Integer -  int - 4 bytes
+//             long - 8 bytes
+//             short - 2 bytes
+//             byte - 1 byte
+//  Float -   float - 4 bytes
+//            double - 8 bytes
+//  char - 2 bytes  
