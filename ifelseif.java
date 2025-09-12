21b78e6d272e6839006ac132f5e5f412ad578538
smriti <hundekarismriti@gmail.com>
2025-09-12 12:10:20 +0530

    Conditinal statements

---
+++ ifelseif.java
@@ -0,0 +1,17 @@
+public class ifelseif {
+    static int x =7;
+    static int y = 8;
+    static int z = 9;
+    public static void main(String[] args){
+     if(x>y && x>z)
+    System.out.println(x);
+     
+     else if(y>z)
+        System.out.println(y);
+     
+     else
+        System.out.println(z);
+     
+    }
+}
+
